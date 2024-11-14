f78452e30266330cd7cccddeddfdfaa5ba49e40f
daniel <danielmayowataiwo@gmail.com>
2024-11-14 02:19:47 +0000

    chore: fix syntax issue in route file

--- src/routes.ts
+++ src/routes.ts
@@ -9,7 +9,7 @@ const SELLER_BASE_PATH = "/api/v1/seller";
 const appRoutes = (app: Application): void => {
   app.use("", () => console.log());
   app.use(BUYER_BASE_PATH, verifyGatewayRequest, buyerRoutes());
-  app.use(SELLER_BASE_PATH, verifyGatewayRequest, sellerRoutes();
+  app.use(SELLER_BASE_PATH, verifyGatewayRequest, sellerRoutes());
 };
 
 export { appRoutes };
