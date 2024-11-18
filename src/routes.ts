c7dcae0b9fd0d3e54ceadd77801e0743be2f3913
daniel <danielmayowataiwo@gmail.com>
2024-11-18 12:14:33 +0000

    chore: remove log console

--- src/routes.ts
+++ src/routes.ts
@@ -7,7 +7,6 @@ const BUYER_BASE_PATH = "/api/v1/buyer";
 const SELLER_BASE_PATH = "/api/v1/seller";
 
 const appRoutes = (app: Application): void => {
-  app.use("", () => console.log());
   app.use(BUYER_BASE_PATH, verifyGatewayRequest, buyerRoutes());
   app.use(SELLER_BASE_PATH, verifyGatewayRequest, sellerRoutes());
 };
